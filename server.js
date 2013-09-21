cb8e3e0cf0f2d41ff8c0f21001bd45e1eb957073
trevorw <ha1ogen@hotmail.com>
2013-09-21 19:25:51 -0400

    changed port and ip variable

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+#!/bin/env node
+
 var app = require('express').createServer(),
     io = require('socket.io').listen(app);
 
 var waitingUsers = [];
 var currentGames = {};
 
-var port = process.env.PORT || 5000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+, ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 app.listen(port);
 
 app.get('/', function (req, res) {
